a22cc63f1a82778462385aab5f474d87bbd68e40
Mohammad Hadi <mhn.hadi@outlook.com>
2022-07-06 10:47:57 +0430

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+<v-app>
+  <v-app-bar app elevation="0" color="white">
+    
+  </v-app-bar>
+
+  <v-main>
+    <router-view/>
+  </v-main>
+</v-app>
+
+
+</template>
+
+
+<style>
+
+</style>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {}
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+});
+</script>
+<style lang="scss">
+@import url('../assets/styles/home.scss');
+</style>
